1b8249e08a5e0596925a8600139b54d0993e0a55
Jhassir Candia <andruu11.jhass@gmail.com>
2018-03-31 05:13:07 -0400

    Camex reestruturado

---
+++ assets/js/carousel-conf.js
@@ -0,0 +1,33 @@
+ $('.events').owlCarousel({
+    loop:true,
+    
+    margin: 0,
+    nav:true,
+    stagePadding: 0,
+    autowidth: false,
+    autoplay:true,
+    autoplayTimeout:1500,
+    navText: ['<i class="fa fa-arrow-circle-left" title="Anterior"></i>','<i class="fa fa-arrow-circle-right" title="Siguiente"></i>'],
+    responsive: {
+        0: {
+            items:1,
+            margin:20
+        },
+        465: {
+            items:2,
+            margin:20,
+            stagePadding:5
+        },
+        992: {
+            items: 3,
+            margin: 1,
+           
+        },
+        1200: {
+            items: 3,
+            margin: 20,
+           
+        }
+    }
+
+})
