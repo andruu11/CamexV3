5ed52103a25f2cc52e1317022d3b6a7845a5e97c
Jhassir Candia <andruu11.jhass@gmail.com>
2018-04-02 06:22:17 -0400

    end page camex

--- assets/js/carousel-conf.js
+++ assets/js/carousel-conf.js
@@ -31,3 +31,38 @@
     }
 
 })
+
+ $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 0,
+    nav:true,
+    stagePadding: 0,
+    autowidth: false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    navText: ['<i class="fa fa-arrow-circle-left" title="Anterior"></i>','<i class="fa fa-arrow-circle-right" title="Siguiente"></i>'],
+    responsive: {
+        0: {
+            items:1,
+            margin:30,
+            stagePadding:30
+        },
+        450: {
+            items:2,
+            margin:20,
+            stagePadding:10
+        },
+        850: {
+            items: 3,
+            margin: 20,
+            stagePadding: 10
+        },
+        992: {
+            items: 3,
+            margin: 100,
+            stagePadding: 10
+        }
+
+    }
+
+})
